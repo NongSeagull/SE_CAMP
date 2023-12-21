053506c6bff0621a3771ec9163d2284d2a9b8d01
NongSeagull <65160230@go.buu.ac.th>
2023-12-21 19:05:26 +0700

    form_101

---
+++ Lab04/form_101.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+        //http://localhost/SE_CAMP/Lab04/index.php
+        <form method="post">
+
+        </form>
+    </body>
+</html>
