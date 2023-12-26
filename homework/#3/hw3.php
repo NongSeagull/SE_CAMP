7e5575bd90289c51fe4efd0ecafaec932ca471b5
NongSeagull <65160230@go.buu.ac.th>
2023-12-26 21:03:15 +0700

    Update hw3.php

---
+++ homework/#3/hw3.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+            <tr>
+                <td>2</td>
+                <td>x</td>
+                <td>1</td>
+                <td>=</td>
+                <td>2</td>
+            </tr>
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
+</body>
+
+</html>
