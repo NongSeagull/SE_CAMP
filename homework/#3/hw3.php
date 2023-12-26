49e89234c6daaabe58237a8a509cdd13f50872a9
NongSeagull <65160230@go.buu.ac.th>
2023-12-26 23:56:45 +0700

    Update hw3.php

--- homework/#3/hw3.php
+++ homework/#3/hw3.php
@@ -1,30 +1,27 @@
 <html>
-
-<head>
-    <meta charset="utf-8">
-</head>
-
-<body>
-    <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-
-    <table>
-        <tbody>
-            <tr>
-                <td>2</td>
-                <td>x</td>
-                <td>1</td>
-                <td>=</td>
-                <td>2</td>
-            </tr>
-        </tbody>
-    </table>
-    <style>
-        table {
-            width: 150px;
-            height: 265px;
-        }
-    </style>
-</body>
-
-</html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="styleSheet" href="styleHW3.css">
+    </head>
+    <body>
+        <?php $multi_x = 2; ?>
+        <h1 class="gradient-border">ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+        <border class="full-withRadius">
+        <div>
+            <table>
+                <tbody>
+                    <?php for ($row = 1; $row < 25; $row ++) { ?>
+                        <tr>
+                            <td><?php echo $multi_x ?></td>
+                            <td>x</td>
+                            <td><?php echo $row?></td>
+                            <td>=</td>
+                            <td><?php echo $multi_x*$row ?></td>
+                            <?php }?>
+                        </tr>
+                    </div>
+                    </border>
+                </tbody>
+            </table>
+        </body>
+        </html>
